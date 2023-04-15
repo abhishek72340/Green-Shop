e3e5bc08e3ba3cd15c6192f11cbd463b7ced3c13
Abhishek Singh Rana <abhisingh.72340@gmail.com>
2023-04-15 12:52:11 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
+import { ToastifyContextProvider } from "./context/clientSide-context";
+import { ProductlistContextProvider } from "./context/productlist-context";
+import { CartContextProvider } from "./context/cart-context";
+import { SidenavContextProvider } from "./context/sidenav-context";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+ 
+        <ToastifyContextProvider>
+          <BrowserRouter>
+            <ProductlistContextProvider>
+            <CartContextProvider>
+            <SidenavContextProvider>
+              <App />
+              </SidenavContextProvider>
+              </CartContextProvider>
+            </ProductlistContextProvider>
+          </BrowserRouter>
+        </ToastifyContextProvider>
+  
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
